3f6b0bf0475a2f05cf9aeefb116cad1edc4014af
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2019-03-05 14:01:10 +0100

    Tika Exercise2

--- Lab2/src/wpzi/Exercise2.java
+++ Lab2/src/wpzi/Exercise2.java
@@ -1,34 +1,40 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+package wpzi;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.tika.Tika;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.detect.Detector;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.langdetect.OptimaizeLangDetector;
+import org.apache.tika.language.detect.LanguageDetector;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-public class Exercise2
-{
+public class Exercise2 {
 
     private static final String DATE_FORMAT = "yyyy-MM-dd";
     private OptimaizeLangDetector langDetector;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Exercise2 exercise = new Exercise2();
         exercise.run();
     }
 
-    private void run()
-    {
-        try
-        {
-            if (!new File("./outputDocuments").exists())
-            {
+    private void run() {
+        try {
+            if (!new File("./outputDocuments").exists()) {
                 Files.createDirectory(Paths.get("./outputDocuments"));
             }
 
@@ -36,39 +42,74 @@ public class Exercise2
 
             File directory = new File("./documents");
             File[] files = directory.listFiles();
-            for (File file : files)
-            {
+            for (File file : files) {
                 processFile(file);
             }
-        } catch (IOException | SAXException | TikaException e)
-        {
+        } catch (IOException | SAXException | TikaException | ParseException e) {
             e.printStackTrace();
         }
 
     }
 
-    private void initLangDetector() throws IOException
-    {
+    private void initLangDetector() throws IOException {
         // TODO initialize language detector (langDetector)
+        this.langDetector = new OptimaizeLangDetector();
     }
 
-    private void processFile(File file) throws IOException, SAXException, TikaException
-    {
+    private void processFile(File file) throws IOException, SAXException, TikaException, ParseException {
         // TODO: extract content, metadata and language from given file
-        // call saveResult method to save the data
 
-        saveResult(file.getName(), null, null, null, null, null, null); //TODO: fill with proper values
+
+        InputStream stream = new FileInputStream(file);
+        LanguageDetector detector = langDetector.loadModels();
+        AutoDetectParser parser = new AutoDetectParser();
+        Metadata metadata = new Metadata();
+        ContentHandler handler = new DefaultHandler();
+        parser.parse(stream, handler, metadata);
+
+        String language = detector.detect(handler.toString()).getLanguage();
+
+        String creator = metadata.get(TikaCoreProperties.CREATOR);
+
+        String creationDate = metadata.get(TikaCoreProperties.CREATED);
+        if (creationDate != null) {
+            Date date = new SimpleDateFormat("yyyy-mm-dd'T'hh:mm:ss'Z'").parse(creationDate);
+            creationDate = new SimpleDateFormat("yyyy-mm-dd").format(date);
+        }
+
+        String lastMod = metadata.get(TikaCoreProperties.MODIFIED);
+
+        AutoDetectParser autoDetectParser = new AutoDetectParser();
+        Detector mimeDetector = autoDetectParser.getDetector();
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(stream);
+        metadata.add(Metadata.RESOURCE_NAME_KEY, file.getName());
+        MediaType mediaType = mimeDetector.detect(bufferedInputStream, metadata);
+
+        stream = new FileInputStream(file);
+        metadata = new Metadata();
+        BodyContentHandler bodyContentHandler = new BodyContentHandler(Integer.MAX_VALUE);
+        autoDetectParser.parse(stream, bodyContentHandler, metadata);
+
+        System.out.println("\nName: " + file.getName());
+        System.out.println("Language: " + language);
+        System.out.println("Creator: " + creator);
+        System.out.println("Creation date: " + creationDate);
+        System.out.println("Last modification: " + lastMod);
+        System.out.println("Mime type: " + mediaType);
+        System.out.println("File content:\n" + bodyContentHandler.toString());
+
+        // call saveResult method to save the data
+        //TODO Daty
+        saveResult(file.getName(), language, creator, new Date(creationDate), new Date(lastMod), mediaType.toString(), bodyContentHandler.toString()); //TODO: fill with proper values
     }
 
     private void saveResult(String fileName, String language, String creatorName, Date creationDate,
-                            Date lastModification, String mimeType, String content)
-    {
+                            Date lastModification, String mimeType, String content) {
 
         SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
         int index = fileName.lastIndexOf(".");
         String outName = fileName.substring(0, index) + ".txt";
-        try
-        {
+        try {
             PrintWriter printWriter = new PrintWriter("./outputDocuments/" + outName);
             printWriter.write("Name: " + fileName + "\n");
             printWriter.write("Language: " + (language != null ? language : "") + "\n");
@@ -81,8 +122,7 @@ public class Exercise2
             printWriter.write("\n");
             printWriter.write(content + "\n");
             printWriter.close();
-        } catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
